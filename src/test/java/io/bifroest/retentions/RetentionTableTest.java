939ed7021220ee70b3c90e9587841ae96073a1b7
Harald Kraemer <tetha42@gmail.com>
2016-01-31 18:14:52 +0100

    Added test for intervals of retention tables

--- src/test/java/io/bifroest/retentions/RetentionTableTest.java
+++ src/test/java/io/bifroest/retentions/RetentionTableTest.java
@@ -3,6 +3,7 @@ package io.bifroest.retentions;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import io.bifroest.commons.model.Interval;
 import org.junit.Test;
 
 public class RetentionTableTest {
@@ -21,5 +22,21 @@ public class RetentionTableTest {
         RetentionTable subject = new RetentionTable(retentionLevel, someBlockIndex);
 
         assertThat(subject.tableName(), is("g" + someLevelName + RetentionTable.SEPARATOR_OF_MADNESS + String.valueOf(someBlockIndex)));
+    }
+
+    @Test
+    public void hasAnAssignedInterval() {
+        String someLevelName = "precise";
+        long someSecondsPerDataPoint = 20;
+        long someBlockNumber = 10;
+        long someBlockSize = 40;
+        String noNextLevel = null;
+        long someBlockIndex = 42;
+
+        RetentionLevel retentionLevel = new RetentionLevel(someLevelName, someSecondsPerDataPoint, someBlockNumber, someBlockSize, noNextLevel);
+
+        RetentionTable subject = new RetentionTable(retentionLevel, someBlockIndex);
+
+        assertThat(subject.getInterval(), is(new Interval(someBlockIndex * someBlockSize, someBlockIndex * someBlockSize + someBlockSize)));
     }
 }
