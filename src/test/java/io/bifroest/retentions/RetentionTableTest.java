695c432df70445238b01f610a123c63491e8941f
Harald Kraemer <tetha42@gmail.com>
2016-01-31 19:10:36 +0100

    Added tests for RetentionTable.canCreateTable

--- src/test/java/io/bifroest/retentions/RetentionTableTest.java
+++ src/test/java/io/bifroest/retentions/RetentionTableTest.java
@@ -2,11 +2,53 @@ package io.bifroest.retentions;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import java.lang.annotation.Retention;
+import java.util.Optional;
 
 import io.bifroest.commons.model.Interval;
 import org.junit.Test;
 
 public class RetentionTableTest {
+
+    @Test
+    public void testMalformedTableNameCannotCreateTable() {
+        RetentionConfiguration configuration = mock(RetentionConfiguration.class);
+        String malformedTableName = "garfield";
+
+        assertThat(RetentionTable.canCreateTable(malformedTableName, configuration), is(false));
+        verifyZeroInteractions(configuration);
+    }
+
+    @Test
+    public void testUnconfiguredTablesCannotBeCreated() {
+        RetentionConfiguration configuration = mock(RetentionConfiguration.class);
+
+        String wellformedTableName = "g" + "level" + RetentionTable.SEPARATOR_OF_MADNESS + "42";
+        when(configuration.getLevelForName("level")).thenReturn(Optional.empty());
+
+        assertThat(RetentionTable.canCreateTable(wellformedTableName, configuration), is(false));
+    }
+
+    @Test
+    public void testConfiguredAndWellFormedNameCanBeCreated() {
+        String someLevelName = "precise";
+        int someSecondsPerDataPoint = 20;
+        int someBlockNumber = 10;
+        int someBlockSize = 40;
+        String noNextLevel = null;
+
+        RetentionLevel retentionLevel = new RetentionLevel(someLevelName, someSecondsPerDataPoint, someBlockNumber, someBlockSize, noNextLevel);
+        String wellformedTableName = "g" + "level" + RetentionTable.SEPARATOR_OF_MADNESS + "42";
+
+        RetentionConfiguration configuration = mock(RetentionConfiguration.class);
+        when(configuration.getLevelForName("level")).thenReturn(Optional.of(retentionLevel));
+
+        assertThat(RetentionTable.canCreateTable(wellformedTableName, configuration), is(true));
+    }
 
     @Test
     public void computesTableNames() {
